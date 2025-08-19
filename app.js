6b5c06d16102b7ea2e7263224f1e9cd19088de71
satekova <sata03@vse.cz>
2025-08-19 17:36:54 +0200

    CRUD, routing

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 
 mongoose.connect('mongodb://localhost:27017/camp')
   .then(() => {
@@ -17,19 +18,48 @@ const port = 3000;
 const path = require('path');
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'))
+app.set('views', path.join(__dirname, 'views'));
 
+app.use(express.urlencoded({extended: true}));
+
+app.use(methodOverride('_method'));
 app.get('/', (req, res) => {
     res.render('home')
 })
 
-app.get('/new', async (req, res) => {
-    const camp = new Campground({
-        title: 'Gerhardhof',
-        description: 'Gerhardhof - Zimmer Glamping Camping in Wildermieming offers family rooms with private bathrooms, balconies, and garden views. Each unit includes a terrace, sofa bed, and modern amenities such as free WiFi, air conditioning, and a work desk.'
-    });
-    await camp.save();
-    res.send(camp);
+app.get('/campgrounds', async (req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render('campgrounds/index', { campgrounds })
+})
+
+app.get('/campgrounds/new', (req,res) => {
+    res.render('campgrounds/new')
+})
+
+app.post('/campgrounds', async(req, res) => {
+    const campground = new Campground(req.body.campground);
+    await campground.save()
+    res.redirect(`/campgrounds/${campground._id}`)
+})
+
+app.get('/campgrounds/:id', async(req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render('campgrounds/details', { campground })
+})
+
+app.get('/campgrounds/:id/edit', async(req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render('campgrounds/edit', { campground})
+})
+app.put('/campgrounds/:id', async(req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground});
+    res.redirect(`/campgrounds/${campground._id}`)
+})
+app.delete('/campgrounds/:id', async(req,res) => {
+    const { id } = req.params;
+    await Campground.findByIdAndDelete(id);
+    res.redirect('/campgrounds')
 })
 
 app.listen(port, () => {
