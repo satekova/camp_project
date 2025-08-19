cf418b38b096ad35932ed68283fe68ac4b69a664
satekova <sata03@vse.cz>
2025-08-19 15:01:17 +0200

    Model, Mongoose connection

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 const express = require('express');
 const app = express();
+
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/camp')
+  .then(() => {
+    console.log("Database connected");
+  })
+  .catch(err => {
+    console.error("connection error:", err);
+  });
+
+const Campground = require('./models/campground')
+
 const port = 3000;
 const path = require('path');
 
@@ -8,6 +21,15 @@ app.set('views', path.join(__dirname, 'views'))
 
 app.get('/', (req, res) => {
     res.render('home')
+})
+
+app.get('/new', async (req, res) => {
+    const camp = new Campground({
+        title: 'Gerhardhof',
+        description: 'Gerhardhof - Zimmer Glamping Camping in Wildermieming offers family rooms with private bathrooms, balconies, and garden views. Each unit includes a terrace, sofa bed, and modern amenities such as free WiFi, air conditioning, and a work desk.'
+    });
+    await camp.save();
+    res.send(camp);
 })
 
 app.listen(port, () => {
