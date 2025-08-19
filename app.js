dad6bc3a7c5096602780531930040228fbcf7738
satekova <sata03@vse.cz>
2025-08-19 14:27:40 +0200

    firt commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const path = require('path');
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.listen(port, () => {
+    console.log(`Lstening on port ${port}`)
+})
