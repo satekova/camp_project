3d8fb083ff5c0be1fb4f0154c7dec62c0a4bfc53
satekova <sata03@vse.cz>
2025-08-22 17:51:17 +0200

    light styling

--- seeds/index.js
+++ seeds/index.js
@@ -18,9 +18,13 @@ const seedDb = async() => {
     await Campground.deleteMany({});
     for(let i = 0; i < 50; i++){
         const random1000 = Math.floor(Math.random()* 1000);
+        const price = Math.floor(Math.random() * 50) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: `https://picsum.photos/400?random=${Math.random()}`,
+            description: '    Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam sint beatae est, possimus, inventore perferendis quidem placeat perspiciatis quaerat magni ipsum, veritatis aperiam explicabo porro quibusdam consequuntur impedit vero debitis?',
+            price
         })
         await camp.save();
     }
